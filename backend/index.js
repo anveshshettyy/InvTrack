6a2d0dba9d6dbcce2d11edce6e27f474968d1972
Anvesh Shetty <shettyanvesh86@gmail.com>
2025-02-01 18:39:16 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -35,11 +35,11 @@ app.use(express.urlencoded({ extended: true }));
 
 // Serve React frontend in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));  
+    app.use(express.static(path.join(__dirname, "./frontend/dist")));  
 
   
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "./frontend", "dist", "index.html"));
     });
 }
 
